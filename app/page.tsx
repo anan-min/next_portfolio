522991688bd16fa981dc6f2a5095b7d1a08c8ea0
anan <nut999anan@hotmail.com>
2024-09-06 13:51:56 +0700

    add shadcn to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
     <section>
       <div className='container'>
-        <h1 className='text-5xl font-bold'></h1>
+        <h1 className='text-5xl font-bold'>test this tailwind</h1>
+        <Button variant="outline">Press this</Button>
       </div>
     </section>
   )
